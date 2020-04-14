eb3c60d751e52f532e0c06a8bbbb4dfd99c47274
Hai <haipro287@gmail.com>
2020-04-14 17:26:00 +0700

    Add project files.

---
+++ PriSchLecs.Frontend/ClientApp/components/app-root.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app" class="container-fluid">
+        hello from app root
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+        },
+
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style>
+
+</style>
