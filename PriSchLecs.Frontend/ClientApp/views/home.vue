0f4bd679a6107c3e1540eb299772ef6c2f2574d5
hoaithu3t <hoaithu3t@gmail.com>
2020-04-23 22:03:21 +0700

    commit home.vue

--- PriSchLecs.Frontend/ClientApp/views/home.vue
+++ PriSchLecs.Frontend/ClientApp/views/home.vue
@@ -1,41 +1,169 @@
-﻿<template>
+﻿<te<template>
     <div>
-        Hello from home
+
+        <h4>GIÁO ÁN ĐƯỢC NHIỀU LƯỢT LIKE</h4>
+        <div>
+            <a-row type="flex">
+                <a-col :span="12" v-for="(card, index) in cards_plan" class="mt-3 pl-5">
+                    <a-icon v-bind:type="card.type_file" v-bind:class="card.type_file" />
+                    <span>{{card.title}}</span>
+                </a-col>
+            </a-row>
+        </div>
+
+        <h4>ĐỀ THI ĐƯỢC DOWNLOAD NHIỀU NHẤT</h4>
+        <div>
+            <a-row type="flex">
+                <a-col :span="12" v-for="(card, index) in cards_test" class="mt-3 pl-5">
+                    <a-icon v-bind:type="card.type_file" v-bind:class="card.type_file" />
+                    <span>{{card.title}}</span>
+                </a-col>
+            </a-row>
+        </div>
+
+        <h4>BÀI GIẢNG ĐƯỢC XEM NHIỀU NHẤT</h4>
+        <div>
+            <a-row type="flex">
+                <a-col :span="6" class="mt-5" v-for="(card, index) in cards_slide">
+                    <a-card hoverable style="width: 250px">
+                        <img alt="example"
+                             v-bind:src="card.img"
+                             slot="cover" />
+                        <h6>{{card.title}}</h6>
+                        <p>{{card.content}}</p>
+                    </a-card>
+                </a-col>
+            </a-row>
+        </div>
+
         <router-link to="/lecture/0">
             <a-button>
                 Bài giảng
             </a-button>
         </router-link>
-        <a-pagination v-model="current" :total="50" showLessItems />
-        <b-button>Button</b-button>
-        <b-button variant="danger">Button</b-button>
-        <b-button variant="success">Button</b-button>
-        <b-button variant="outline-primary">Button</b-button>7
-        <b-row>
-            <b-col md="auto">
-                <b-calendar v-model="value" @context="onContext" locale="en-US"></b-calendar>
-            </b-col>
-            <b-col>
-                <p>Value: <b>'{{ value }}'</b></p>
-                <p class="mb-0">Context:</p>
-                <pre class="small">{{ context }}</pre>
-            </b-col>
-        </b-row>
+
     </div>
 </template>
 <script>
     export default {
         data() {
             return {
-                current: 0
-            }
+                current: 0,
+                cards_slide: [
+                    {
+                       img : "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+                        title: 'Toan lop 1',
+                        content : 'cong tu nhan chia so 1 chu so'
+                    },
+                    {
+                       img : "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+                        title: 'Toan lop 2',
+                        content : 'cong tu nhan chia so 1 chu so'
+                    },
+                    {
+                       img : "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+                        title: 'Toan lop 3',
+                        content : 'cong tu nhan chia so 1 chu so'
+                    },
+                    {
+                       img : "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+                        title: 'Toan lop 3',
+                        content : 'cong tu nhan chia so 1 chu so'
+                    },
+                    {
+                       img : "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+                        title: 'Toan lop 3',
+                        content : 'cong tu nhan chia so 1 chu so'
+                    },
+                    {
+                       img : "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+                        title: 'Toan lop 3',
+                        content : 'cong tu nhan chia so 1 chu so'
+                    },
+                    {
+                       img : "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+                        title: 'Toan lop 3',
+                        content : 'cong tu nhan chia so 1 chu so'
+                    },
+                    {
+                        img: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+                        title: 'Toan lop 4',
+                        content : 'cong tu nhan chia so 2 chu so'
+                    }
+                ],
+                cards_test: [
+                         {
+                        type_file:"file-word",
+                        title: "Đề thi giữa học kì 1 môn tiếng việt lớp 2"
+                    },
+                    {
+                        type_file: "file-excel",
+                        title: "Đề thi giữa học kì 2 môn tiếng việt lớp 3"
+                    },
+                    {
+                        type_file: "file-word",
+                        title: "Đề thi giữa học kì 1 môn toán lớp 5"
+                    },
+                    {
+                        type_file: "file-excel",
+                        title: "Đề thi giữa học kì 1 môn tiếng anh lớp 4"
+                    },
+                    {
+                        type_file: "file-excel",
+                        title: "Đề thi giữa học kì 2 môn lịch sử lớp 5"
+                    },
+                    {
+                        type_file: "file-word",
+                        title: "Đề thi giữa học kì 1 môn tiếng việt lớp 2"
+                    }
+                ],
+                cards_plan: [
+                         {
+                        type_file:"file-word",
+                        title: "Giáo án toán 2 học kì I"
+                    },
+                    {
+                        type_file: "file-excel",
+                        title: "Giáo án TH&XH lớp 2"
+                    },
+                    {
+                        type_file: "file-excel",
+                        title: "Giáo án môn toán lớp 5 kì I"
+                    },
+                    {
+                        type_file: "file-word",
+                        title: "Giáo án tiếng anh lớp 4 cả năm"
+                    },
+                    {
+                        type_file: "file-excel",
+                        title: "Giáo án học kì 2 môn lịch sử lớp 5 cả năm"
+                    },
+                    {
+                        type_file: "file-word",
+                        title: "Giáo án môn tiếng việt lớp 2 kì I"
+                    }
+                ]
+
+            };
         },
         methods: {
-            goToLecture() {
+             goToLecture() {
                 this.$router.replace('/lecture/0');
             }
         }
     }
 </script>
 <style>
+    .file-word {
+        transform: translateY(-5px);
+        color : blue  !important;
+    }
+    .file-excel{
+        transform: translateY(-5px);
+        color: orangered !important;
+    }
+    h4 {
+        text-align: center;
+        padding: 50px 0px;
+    }
 </style>
